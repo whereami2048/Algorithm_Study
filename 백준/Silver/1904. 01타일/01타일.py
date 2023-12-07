ef9d398dee2e038d796de4b14864512337d84577
Yong-Hyeon <104913654+whereami2048@users.noreply.github.com>
2023-12-07 18:46:18 +0900

    [Silver III] Title: 01타일, Time: 364 ms, Memory: 70088 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.py
@@ -0,0 +1,20 @@
+import sys
+
+n = int(input())
+
+if n == 1:
+    print(1)
+    sys.exit()
+elif n == 2:
+    print(2)
+    sys.exit()
+
+dp = [0 for _ in range(n + 1)]
+
+dp[1] = 1
+dp[2] = 2
+
+for i in range(3, n + 1):
+    dp[i] = (dp[i-1] + dp[i-2]) % 15746
+
+print(dp[n])
