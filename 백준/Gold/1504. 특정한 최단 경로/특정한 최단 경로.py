1c7185a091cf73778755f9e8678a02b4f34d6540
Yong-Hyeon <104913654+whereami2048@users.noreply.github.com>
2024-07-01 18:27:40 +0900

    Update 특정한 최단 경로.py

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.py
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.py
@@ -1,31 +1,44 @@
 import sys
 import heapq
 input = sys.stdin.readline
+N, E = map(int, input().split())
+
+graph = [[] for _ in range(N + 1)]
+
+for _ in range(E):
+    a, b, c = map(int, input().split())
+    graph[a].append((b, c))
+    graph[b].append((a, c))
+
+v1, v2 = map(int, input().split())
+
 INF = sys.maxsize
 
-N, E = map(int, input().split())
-graph = [[] for _ in range(N+1)]
-for _ in range(E):
-    a,b,c = map(int,input().split())
-    graph[a].append((b,c))
-    graph[b].append((a,c))
-v1,v2 = map(int,input().split())
 
-def dijkstra(start,end):
-    dist = [INF]*(N+1)
-    dist[start] = 0
-    hq = [(0,start)]
-    while hq:
-        len,node = heapq.heappop(hq)
-        if len > dist[node]:
+def dijkstra(start_node, end_node):
+    distance = [INF] * (N + 1)
+    distance[start_node] = 0
+    queue = []
+    heapq.heappush(queue, (start_node, 0))
+
+    while queue:
+        current_node, dist = heapq.heappop(queue)
+
+        if distance[current_node] < dist:
             continue
-        for next_node, val in graph[node]:
-            if dist[next_node] > dist[node] + val:
-                dist[next_node] = dist[node]+val
-                heapq.heappush(hq,(dist[next_node],next_node))
-    return dist[end]
 
-path1 = dijkstra(1,v1) + dijkstra(v1, v2) + dijkstra(v2,N)
-path2 = dijkstra(1,v2) + dijkstra(v2, v1) + dijkstra(v1,N)
+        for next_node, weight in graph[current_node]:
+            if distance[next_node] > distance[current_node] + weight:
+                distance[next_node] = distance[current_node] + weight
+                heapq.heappush(queue, (next_node, distance[next_node]))
 
-print(-1) if path1 >= INF and path2 >= INF else print(min(path1,path2))
+    return distance[end_node]
+
+
+p1 = dijkstra(1, v1) + dijkstra(v1, v2) + dijkstra(v2, N)
+p2 = dijkstra(1, v2) + dijkstra(v2, v1) + dijkstra(v1, N)
+
+if p1 >= INF and p2 >= INF:
+    print(-1)
+else:
+    print(min(p1, p2))
