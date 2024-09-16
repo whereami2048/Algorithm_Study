e4b50b1c5cc603241fff7424219e4dc79ad6e102
Yong-Hyeon <104913654+whereami2048@users.noreply.github.com>
2024-09-16 17:59:02 +0900

    [Gold I] Title: 구슬 탈출 2, Time: 68 ms, Memory: 34156 KB -BaekjoonHub

---
+++ 백준/Gold/13460. 구슬 탈출 2/구슬 탈출 2.py
@@ -0,0 +1,74 @@
+from collections import deque
+import sys
+
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+graph = []
+for i in range(n):
+    graph.append(list(input()))
+    for j in range(m):
+        if graph[i][j] == 'R':
+            red_x, red_y = i, j
+        elif graph[i][j] == 'B':
+            blue_x, blue_y = i, j
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+
+def bfs(rx, ry, bx, by):
+    q = deque()
+    q.append((rx, ry, bx, by))
+    visited = []
+    visited.append((rx, ry, bx, by))
+    count = 0
+
+    while q:
+        for _ in range(len(q)):
+            rx, ry, bx, by = q.popleft()
+            if count > 10:
+                print(-1)
+                return
+            if graph[rx][ry] == 'O':
+                print(count)
+                return
+            for i in range(4):
+                nrx, nry = rx, ry
+                while True:
+                    nrx += dx[i]
+                    nry += dy[i]
+                    if graph[nrx][nry] == '#':
+                        nrx -= dx[i]
+                        nry -= dy[i]
+                        break
+                    if graph[nrx][nry] == 'O':
+                        break
+                nbx, nby = bx, by
+                while True:
+                    nbx += dx[i]
+                    nby += dy[i]
+                    if graph[nbx][nby] == '#':
+                        nbx -= dx[i]
+                        nby -= dy[i]
+                        break
+                    if graph[nbx][nby] == 'O':
+                        break
+                if graph[nbx][nby] == 'O':
+                    continue
+                if nrx == nbx and nry == nby:
+                    if abs(nrx - rx) + abs(nry - ry) > abs(nbx - bx) + abs(
+                            nby - by):
+                        nrx -= dx[i]
+                        nry -= dy[i]
+                    else:
+                        nbx -= dx[i]
+                        nby -= dy[i]
+                if (nrx, nry, nbx, nby) not in visited:
+                    q.append((nrx, nry, nbx, nby))
+                    visited.append((nrx, nry, nbx, nby))
+        count += 1
+    print(-1)
+
+
+bfs(red_x, red_y, blue_x, blue_y)
