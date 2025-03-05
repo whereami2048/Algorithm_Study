f9e89ea7e16f159da8aba63d3686c1fa03855888
Yong-Hyeon <104913654+whereami2048@users.noreply.github.com>
2025-03-05 15:54:29 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 265.37 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.py
@@ -0,0 +1,26 @@
+def solution(n, k):
+    word = ""
+    while n:
+        word = str(n % k) + word
+        n = n // k
+        
+    word = word.split("0")  
+    count = 0
+    
+    for w in word:
+        if len(w) == 0:
+            continue
+            
+        if int(w) < 2:
+            continue
+            
+        is_prime=True
+        
+        for i in range(2,int(int(w)**0.5)+1):
+            if int(w)%i==0:
+                is_prime=False
+                break
+        if is_prime:
+            count+=1
+            
+    return count
